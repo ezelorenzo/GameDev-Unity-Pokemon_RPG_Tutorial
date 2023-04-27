16785fde987e50764c2ac758dc14952a9cf88445
ezelorenzomol@gmail.com <ezelorenzomol@gmail.com>
2023-04-26 22:13:12 -0300

    Battle system action and move selection and moves fix

--- Assets/Scripts/Battle/BattleDialogBox.cs
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -6,6 +6,14 @@ public class BattleDialogBox : MonoBehaviour
 {
     [SerializeField] int lettersPerSecond;
     [SerializeField] Text dialogText;
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+    [SerializeField] Color highlightedColor;
 
     public void SetDialogText(string dialog)
     {
@@ -20,7 +28,71 @@ public class BattleDialogBox : MonoBehaviour
         foreach (char letter in dialog)
         {
             dialogText.text += letter;
-            yield return new WaitForSeconds(1f/ lettersPerSecond);
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for(int i=0; i<actionTexts.Count;i++)
+        {
+            if(i==selectedAction)
+            {
+                actionTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                actionTexts[i].color = Color.black;
+            }
+        }    
+    }
+
+    public void UpdateMoveSelection(int selectedMove, Move move)
+    {
+        for (int i = 0; i < moveTexts.Count; ++i)
+        {
+            if (i == selectedMove)
+            {
+                moveTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                moveTexts[i].color = Color.black;
+            }
+        }
+
+        ppText.text = $"PP {move.PP}/{move.Base.PP}";
+        typeText.text = move.Base.Type.ToString();
+    }
+
+    public void SetMoveNames(List<Move> moves)
+    {
+        for (int i = 0; i < moveTexts.Count; i++)
+        {
+            if (i<moves.Count)
+            {
+                moveTexts[i].text = moves[i].Base.name;
+            }
+            else
+            {
+                moveTexts[i].text = "-";
+            }
         }
     }
 }
